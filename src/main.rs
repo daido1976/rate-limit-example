e6d06fed1c5557274d28343115960a6002f0488e
daido1976 <daido1976@gmail.com>
2023-07-15 21:08:24 +0900

    Introduce dynamodb example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use aws_sdk_dynamodb::{Client, Error};
+
+/// Lists your DynamoDB tables in the default Region or us-east-1 if a default Region isn't set.
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let config = aws_config::from_env()
+        .profile_name("local")
+        .endpoint_url("http://localhost:8000")
+        .load()
+        .await;
+
+    let client = Client::new(&config);
+
+    let resp = client.list_tables().send().await?;
+
+    println!("Tables:");
+
+    let names = resp.table_names().unwrap_or_default();
+
+    for name in names {
+        println!("  {}", name);
+    }
+
+    println!();
+    println!("Found {} tables", names.len());
+
+    Ok(())
 }
