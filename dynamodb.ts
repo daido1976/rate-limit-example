28463830e67b72c918bd6f1ab0e9f8dec752362b
daido1976 <daido1976@gmail.com>
2023-07-16 12:41:21 +0900

    Impl rate limiter with deno

--- dynamodb.ts
+++ dynamodb.ts
@@ -1,25 +1,50 @@
 import rateLimitTable from "./scripts/rate-limit-table.json" assert { type: "json" };
 import {
   DynamoDBClient,
-  ListTablesCommand,
+  UpdateItemCommand,
 } from "npm:@aws-sdk/client-dynamodb";
 import { fromIni } from "npm:@aws-sdk/credential-provider-ini";
 
-// クライアントの設定を行います
 const client = new DynamoDBClient({
   region: "us-west-2",
   credentials: fromIni({ profile: "local" }),
   endpoint: "http://localhost:8000",
 });
 
-const run = async () => {
-  const command = new ListTablesCommand({});
-  try {
-    const results = await client.send(command);
-    console.log("Table names are ", results.TableNames);
-  } catch (err) {
-    console.error(err);
-  }
-};
+async function isRateLimited(userId: string) {
+  // 今日の日付を取得
+  const date = new Date().toISOString().split("T")[0];
 
-run();
+  const command = new UpdateItemCommand({
+    TableName: rateLimitTable.TableName,
+    Key: {
+      userId: { S: userId },
+      date: { S: date },
+    },
+    ExpressionAttributeNames: {
+      "#count": "count",
+      "#ttl": "expiredAt",
+    },
+    ExpressionAttributeValues: {
+      ":inc": { N: "1" },
+      ":ttl": { N: (Math.floor(Date.now() / 1000) + 24 * 60 * 60).toString() }, // TTLのタイムスタンプ（24時間後のUNIX時間）
+    },
+    UpdateExpression: "ADD #count :inc SET #ttl = :ttl",
+    ReturnValues: "UPDATED_NEW",
+  });
+
+  const result = await client.send(command);
+
+  return parseInt(result.Attributes?.count?.N || "0") > 15;
+}
+
+async function handleRequest(userId: string) {
+  if (await isRateLimited(userId)) {
+    return "Rate limit exceeded. Try again tomorrow.";
+  } else {
+    // process the request normally
+    return "Request processed.";
+  }
+}
+
+handleRequest("user1").then(console.log);
