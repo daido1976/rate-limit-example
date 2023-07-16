3b09332ba0f077752cc4009a52942cf36b7a13d4
daido1976 <daido1976@gmail.com>
2023-07-16 17:32:23 +0900

    Impl redis rate limiter

---
+++ redis/rateLimit.ts
@@ -0,0 +1,34 @@
+import { createClient } from "npm:redis";
+import { format } from "https://deno.land/std@0.194.0/datetime/format.ts";
+
+const client = createClient({
+  url: "redis://localhost:6380",
+});
+
+await client.connect();
+
+async function isRateLimited(userId: string): Promise<boolean> {
+  const date = format(new Date(), "yyyy-MM-dd");
+  const key = `rate_limit:${userId}:${date}`;
+
+  const m = client.multi();
+  m.incr(key);
+  m.expire(key, 24 * 60 * 60); // TTL: 24時間
+  const responses = await m.exec();
+  const currentCount = responses[0][1];
+
+  return currentCount > 100;
+}
+
+async function handleRequest(userId: string): Promise<string> {
+  if (await isRateLimited(userId)) {
+    return "Rate limit exceeded. Try again tomorrow.";
+  } else {
+    // process the request normally
+    return "Request processed.";
+  }
+}
+
+await handleRequest("user1")
+  .then(console.log)
+  .finally(() => client.quit());
